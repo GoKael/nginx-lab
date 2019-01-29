9b1610ae1e956de5decf83fb4c50f7c9738d2ee8
yanzhiwang <yanzhi716@gmail.com>
2019-01-29 05:45:55 -0500

    first commit

---
+++ checknginx.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+rootPath=$1
+nginxVersion=1.14  # Could also be passed as an argument using $2
+
+result=$(docker run --rm -t -a stdout --name my-nginx -v ${rootPath}:/etc/nginx/:ro nginx:$nginxVersion nginx -c /etc/nginx/nginx.conf -t)
+
+# Look for the word successful and count the lines that have it
+# This validation could be improved if needed
+successful=$(echo $result | grep successful | wc -l)
+
+if [ $successful = 0 ]; then
+    echo FAILED
+    echo "$result"
+    exit 1
+else
+    echo SUCCESS
+fi
